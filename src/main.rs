b426e0b8c9dd905f43f414a739b8743e0c150258
Lukas Meili <luk.meili@bluewin.ch>
2024-01-31 16:06:17 +0100

    Add dancing animation

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ use std::io::Cursor;
 use stl_io::IndexedMesh;
 
 const FPS: f32 = 30.0;
+const ANIMATION_SPEED: f64 = 0.2 ;
+const ANIMATION_REACHED_THRESHOLD: f64 = 0.01;
 
 fn main() {
     // get environment variables
@@ -48,10 +50,9 @@ fn main() {
     let fov = 90.0;
     let mut transform_matrix = Transform3D::new_trs(
         Vec3D::new(15.0, -14.0, 110.0),
-        Vec3D::new(0.0, 1.5, -1.5),
+        Vec3D::new(2.0, 1.5, -1.5),
         Vec3D::new(0.3, 0.3, 0.3),
     );
-    // let display_mode = DisplayMode::Wireframe { backface_culling: true };
     let light = Light {
         intensity: 1.2,
         light_type: LightType::Point {
@@ -62,12 +63,110 @@ fn main() {
         lights: vec![light],
     };
     let meshes: Vec<&dyn ViewElement3D> = vec![&mesh];
+    let mut animation_state = AnimationState::Roatation;
+
+    let mut animation_change_counter = 0;
+    let mut counter = 0;
 
     loop {
         view.clear();
 
+        match animation_state {
+            AnimationState::LeftWhippingUp => {
+                let target_rotation = Vec3D::new(2.0, 1.5, -1.4);
+                // calculate the rotation to the target rotation
+                let rotation = target_rotation - transform_matrix.rotation;
+                // calculate the rotation speed
+                let rotation_speed = rotation * ANIMATION_SPEED;
+                // add the rotation speed to the current rotation
+                transform_matrix.rotation += rotation_speed;
+                // check if the rotation is close enough to the target rotation
+                if rotation_speed.magnitude() < ANIMATION_REACHED_THRESHOLD {
+                    // set the rotation to the target rotation
+                    transform_matrix.rotation = target_rotation;
+                    // set the animation state to the next state
+                    counter += 1;
+                    if counter > 2 {
+                        animation_state = AnimationState::RightWhippingUp;
+                        counter = 0;
+                        animation_change_counter += 1;
+                        if animation_change_counter > 2 {
+                            animation_state = AnimationState::Roatation;
+                            animation_change_counter = 0;
+                        }
+                    } else {
+                        animation_state = AnimationState::LeftWhippingDown;
+                    }
+                }
+            }
+            AnimationState::LeftWhippingDown => {
+                let target_rotation = Vec3D::new(2.2, 1.5, -1.7);
+                // calculate the rotation to the target rotation
+                let rotation = target_rotation - transform_matrix.rotation;
+                // calculate the rotation speed
+                let rotation_speed = rotation * ANIMATION_SPEED;
+                // add the rotation speed to the current rotation
+                transform_matrix.rotation += rotation_speed;
+                // check if the rotation is close enough to the target rotation
+                if rotation_speed.magnitude() < ANIMATION_REACHED_THRESHOLD {
+                    // set the rotation to the target rotation
+                    transform_matrix.rotation = target_rotation;
+                    // set the animation state to the next state
+                    animation_state = AnimationState::LeftWhippingUp;
+                }
+            }
+            AnimationState::RightWhippingUp => {
+                let target_rotation = Vec3D::new(2.5, 1.5, -1.4);
+                // calculate the rotation to the target rotation
+                let rotation = target_rotation - transform_matrix.rotation;
+                // calculate the rotation speed
+                let rotation_speed = rotation * ANIMATION_SPEED;
+                // add the rotation speed to the current rotation
+                transform_matrix.rotation += rotation_speed;
+                // check if the rotation is close enough to the target rotation
+                if rotation_speed.magnitude() < ANIMATION_REACHED_THRESHOLD {
+                    // set the rotation to the target rotation
+                    transform_matrix.rotation = target_rotation;
+                    // set the animation state to the next state
+                    counter += 1;
+                    if counter > 2 {
+                        animation_state = AnimationState::LeftWhippingUp;
+                        counter = 0;
+                    } else {
+                        animation_state = AnimationState::RightWhippingDown;
+                    }
+                }
+            }
+            AnimationState::RightWhippingDown => {
+                let target_rotation = Vec3D::new(2.2, 1.5, -1.7);
+                // calculate the rotation to the target rotation
+                let rotation = target_rotation - transform_matrix.rotation;
+                // calculate the rotation speed
+                let rotation_speed = rotation * ANIMATION_SPEED;
+                // add the rotation speed to the current rotation
+                transform_matrix.rotation += rotation_speed;
+                // check if the rotation is close enough to the target rotation
+                if rotation_speed.magnitude() < ANIMATION_REACHED_THRESHOLD {
+                    // set the rotation to the target rotation
+                    transform_matrix.rotation = target_rotation;
+                    // set the animation state to the next state
+                    animation_state = AnimationState::RightWhippingUp;
+                }
+            }
+            AnimationState::Roatation => {
+                transform_matrix.rotation.z = -1.5;
+                transform_matrix.rotation.x += 0.2;
+                // if the cow has fully rotated we need to modulo the rotation
+                transform_matrix.rotation.x %= 6.28;
+                counter += 1;
+                if counter > 62 {
+                    animation_state = AnimationState::LeftWhippingDown;
+                    counter = 0;
+                }
+            }
+        }
+
         // Increase the rotation of the mesh around the y-axis by 0.1 radians
-        transform_matrix.rotation.x += 0.1;
         let view_port = Viewport::new(transform_matrix, fov, screen_origin);
         let pixels = view_port.render(meshes.to_owned(), display_mode.to_owned());
         view.blit(&pixels, Wrapping::Ignore);
@@ -92,4 +191,12 @@ fn stl_to_3d_mesh(stl_file: &IndexedMesh) -> Mesh3D {
         .collect();
 
     Mesh3D::new(Transform3D::default(), vertices, faces)
+}
+
+enum AnimationState {
+    LeftWhippingDown,
+    LeftWhippingUp,
+    RightWhippingDown,
+    RightWhippingUp,
+    Roatation,
 }
